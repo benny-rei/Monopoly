3bc3c9b666ac3f57990c116b8881d453fb50fc87
bennny <bennny@bennny-Satellite-L550>
2017-04-15 16:49:38 +0200

    Signed-off-by: bennny <bennny@bennny-Satellite-L550>

--- Monopoly/src/Dice.java
+++ Monopoly/src/Dice.java
@@ -1,12 +1,15 @@
 
 public class Dice {
 	
-	public int rollDice(){
+	public static int rollDice(){
 		
 		int wuerfel1 = 0, wuerfel2 = 0;
 		
 		wuerfel1 = (int) ((6*Math.random())+1);
 		wuerfel2 = (int) ((6*Math.random())+1);
+		
+		if(wuerfel1 == wuerfel2)
+			return 0;
 		
 		return (wuerfel1 + wuerfel2);
 		
--- Monopoly/src/Game.java
+++ Monopoly/src/Game.java
@@ -1,83 +1,102 @@
+import java.util.ArrayList;
 
 public class Game {
-	
-	private Player[] players;
+
+	private static Player[] players;
 	private static Board board;
-	private Dice[] dices;
-	
-	
-	public static void main(String[] args) {	
-		
+	private static Dice[] dices;
+	private final static int startkapital = 500;
+	private static int anzahl;
+	private static int aktiverSpieler;
+
+	private static int pasch = 0;
+
+	public static void main(String[] args) {
+
 		/**
-		 * 1. player erstellen
-		 * 2. board füllen
-		 * 3. 
+		 * 1. player erstellen 2. board füllen 3.
 		 * 
 		 */
-		
-		
+
+		// anzahl der Spieler festlegen
+		anzahl = 2;
+		aktiverSpieler = 0;
+
+		players = new Player[2];
+
+		players[0] = new Player("Bennsch", startkapital, 0, null, null);
+		players[1] = new Player("Berglo", startkapital, 0, null, null);
+
 		board = new Board();
 		board.fillBoard();
-		
+
+		while (true) {
+
+			// ---- Menü ------
+
+			// 1. Würfeln
+
+			int wuerfel = Dice.rollDice();
+			if ((wuerfel == 0) && (pasch < 3)) {
+				System.out.println("Sie haben einen Pasch gewürfelt");
+				pasch++;
+
+			} else {
+
+				pasch = 0;
+				if (players[aktiverSpieler].getPosition() == 10) {
+					players[aktiverSpieler].setPosition(10); // ins gefängnis gehen
+					AktiverSpielerAendern();
+				}
+				continue;
+				
+			}
+
+			System.out.println("Was wollen Sie machen?");
+
+			// ende --- aktiven Spieler bestimmen
+			AktiverSpielerAendern();
+		}
+
 	}
-	
-	
-	
+
+	public static void AktiverSpielerAendern() {
+		if (aktiverSpieler < (anzahl - 1))
+			aktiverSpieler++;
+		else
+			aktiverSpieler = 0;
+	}
 
 	public Player[] getPlayers() {
 		return players;
 	}
 
-
-
-
-
 	public void setPlayers(Player[] players) {
 		this.players = players;
 	}
-
-
-
-
 
 	public Board getBoard() {
 		return board;
 	}
 
-
-
-
-
 	public void setBoard(Board board) {
 		this.board = board;
 	}
-
-
-
-
 
 	public Dice[] getDices() {
 		return dices;
 	}
 
-
-
-
-
 	public void setDices(Dice[] dices) {
 		this.dices = dices;
 	}
 
-
-
-
-
 	public void start() {
-		
+
 	}
-	
-	public void move(Player player){
-		
+
+	public void move(Player player) {
+
 	}
-	
+
 }
